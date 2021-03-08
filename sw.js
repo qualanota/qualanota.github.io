c1309ae545095d0172fe0015fd52f98dd3a0cf7d
qualanota <qualanota@protonmail.com>
2021-03-08 14:24:21 -0300

    fixed slider

--- sw.js
+++ sw.js
@@ -46,7 +46,7 @@ var __wpo = {
       "/ca00542358747732ab84b207f56ac54b.jpg",
       "/3388fd2c2f2de65bf67327a5d483c0c8.jpg",
       "/c9dd69953af7a0540c9a6cdc41115325.webp",
-      "/runtime.f5f93e897dce97677a21.js",
+      "/runtime.eb52123d42dd1870a1e1.js",
       "/"
     ],
     "additional": [
@@ -64,7 +64,7 @@ var __wpo = {
       "/npm.prop-types.00bbae956717cc1050e2.chunk.js",
       "/npm.react-app-polyfill.83ebef0577be4f8cc587.chunk.js",
       "/npm.react-intl.741dd49add785b9d27b0.chunk.js",
-      "/15.ad30fd902d99be478bf6.chunk.js",
+      "/15.f5be83ad214c3ce47e90.chunk.js",
       "/16.6034045e6580999510bb.chunk.js",
       "/17.d6848cc7cc19eb7fbec4.chunk.js"
     ],
@@ -131,15 +131,15 @@ var __wpo = {
     "9f1252d482503a85c26e3649d66c83f18e153a18": "/npm.prop-types.00bbae956717cc1050e2.chunk.js",
     "9a19949ba5dcba89c5462a4acaa175558648c583": "/npm.react-app-polyfill.83ebef0577be4f8cc587.chunk.js",
     "1a47665794ea6da17dc74a8e96924a52b1152865": "/npm.react-intl.741dd49add785b9d27b0.chunk.js",
-    "dbd3ab92091fa7b00e4e4bcbaea7aa6286a4669b": "/runtime.f5f93e897dce97677a21.js",
-    "bbdc09863288be25e0df09cb442741c49c7ab7a5": "/15.ad30fd902d99be478bf6.chunk.js",
+    "62b3b61b2882bd9b4f5590d1682fd03cb844012b": "/runtime.eb52123d42dd1870a1e1.js",
+    "970c3537d80d22245a53ed65a3f121edda624c55": "/15.f5be83ad214c3ce47e90.chunk.js",
     "5538539ce0d4e3d0252a7452688e1dad132d6102": "/16.6034045e6580999510bb.chunk.js",
     "73361e27c453f7b1dd0e0150c1885f3d324eeefa": "/17.d6848cc7cc19eb7fbec4.chunk.js",
-    "61c9a26b079994d963bec3d0895d8b2e619244d3": "/"
+    "f9588f528311313abf82aeae4dc20dd0b8e3ca43": "/"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "3/8/2021, 2:01:20 PM",
+  "version": "3/8/2021, 2:24:03 PM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
